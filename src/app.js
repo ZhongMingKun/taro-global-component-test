34fff182380ca82bfb68c9f7b19df54d2751d3df
zhongmingkun <zhongmingkun@ut.cn>
2019-07-11 10:16:18 +0800

    feat(pages): 添加新页面及tabBar图片

--- src/app.js
+++ src/app.js
@@ -13,10 +13,30 @@ class App extends Component {
 
   config = {
     pages: [
-      'pages/index/index'
+      'pages/index/index',
+      'pages/msg/index'
     ],
     usingComponents: {
-      'van-button': 'components/vant-weapp/button/index'
+      'van-button': './components/vant-weapp/button/index'
+    },
+    tabBar: {
+      "color": "#333",
+      "selectedColor": "#00C2C7",
+      "backgroundColor": "#fff",
+      "borderStyle": "black",
+      "list": [{
+          "pagePath": "pages/index/index",
+          "text": "首页",
+          "iconPath": "static/images/home.png",
+          "selectedIconPath": "static/images/home_active.png"
+        },
+        {
+          "pagePath": "pages/msg/index",
+          "text": "消息",
+          "iconPath": "static/images/msg.png",
+          "selectedIconPath": "static/images/msg_active.png"
+        }
+      ]
     },
     window: {
       backgroundTextStyle: 'light',
